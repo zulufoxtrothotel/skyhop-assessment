e0d05a9998e3de52b061d87b43dc49ca2c90208c
zhulett <zhulett@gmail.com>
2024-02-13 22:54:55 -0600

    remove console.log

--- src/index.tsx
+++ src/index.tsx
@@ -5,7 +5,7 @@ import App from './App'
 import reportWebVitals from './reportWebVitals'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
-console.log('render')
+
 root.render(
     <React.StrictMode>
         <App />
